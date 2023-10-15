16edff3a6ffd8872cf69825261abeb9d1fc3be5a
andre.borba <dev_andre.filipe@outlook.com>
2023-10-15 19:09:23 -0300

    Initial Commit

---
+++ YouBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouBank
+//
+//  Created by André Filipe Fonsêca Borba on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouBank/YouBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouBankApp.swift
+//  YouBank
+//
+//  Created by André Filipe Fonsêca Borba on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YouBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
